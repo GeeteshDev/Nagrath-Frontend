ff51ae8b3d3d750b6a199a782fd6c99f22945aa7
Geetesh Aheriya <geeteshaheriya506@gmail.com>
2025-02-21 12:11:01 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["xlsx"],
+  },
+  build: {
+    rollupOptions: {
+      external: ["fs"], // If using `xlsx` in the backend, exclude `fs`
+    },
+  },
 })
